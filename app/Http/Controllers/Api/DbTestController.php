64f09895f9c806f66de68456abbbfed179907e09
Roger Alejandro Gomez Sanabria <rogergomezs2003@gmail.com>
2026-03-11 17:17:57 -0400

    Add empty API DB test controller

---
+++ app/Http/Controllers/Api/DbTestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class DbTestController extends Controller
+{
+    // Controlador base para pruebas de conexión a BD.
+}
