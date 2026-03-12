5b5e37ff82d41775ffa81d5a9cd4666da7fbb268
Roger Alejandro Gomez Sanabria <rogergomezs2010@outlook.com>
2026-03-12 01:00:45 -0400

    se corrige apuntar rutas

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,10 @@ use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Api\SwaggerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/ingresar', [LoginController::class, 'ingresar'])->name('ingresar');
 Route::get('/auth_google', [LoginController::class, 'authGoogle'])->name('auth.google');
-Route::get('/home', [HomeController::class, 'home'])->name('home');
 
 Route::get('/api', [SwaggerController::class, 'ui'])->name('swagger.ui');
 Route::get('/api/openapi.json', [SwaggerController::class, 'spec'])->name('swagger.spec');
