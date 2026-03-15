6bed5a25c0baa5f1cf676850d97da37e7319075f
Roger Alejandro Gomez Sanabria <rogergomezs2003@gmail.com>
2026-03-15 02:08:13 -0400

    Cambiar endpoint api_token a GET

--- app/Http/Controllers/Api/ApiTokenController.php
+++ app/Http/Controllers/Api/ApiTokenController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiToken;
+use Illuminate\Http\JsonResponse;
 
 class ApiTokenController extends Controller
 {
-    //
+    public function apiToken(): JsonResponse
+    {
+        $respuesta = ApiToken::obtenerToken();
+
+        return response()->json([
+            'codigo' => $respuesta->codigo,
+            'mensaje' => $respuesta->mensaje,
+            'data' => $respuesta->data,
+        ]);
+    }
 }
