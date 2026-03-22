87222c2083b1c9a51048f79dfbe91af1f8c28b75
Roger Alejandro Gomez Sanabria <rogergomezs2003@gmail.com>
2026-03-22 17:00:10 -0400

    fix: restaurar métodos en LoginController y retirar AuthController

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\LoginController;
 use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Api\SwaggerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/ingresar', [AuthController::class, 'ingresar'])->name('ingresar');
-Route::get('/auth_google/status', [AuthController::class, 'googleStatus'])->name('auth.google.status');
-Route::post('/auth_google', [AuthController::class, 'authGoogle'])->name('auth.google');
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/ingresar', [LoginController::class, 'ingresar'])->name('ingresar');
+Route::get('/auth_google/status', [LoginController::class, 'googleStatus'])->name('auth.google.status');
+Route::post('/auth_google', [LoginController::class, 'authGoogle'])->name('auth.google');
 
 Route::get('/api', [SwaggerController::class, 'ui'])->name('swagger.ui');
 Route::get('/api/openapi.json', [SwaggerController::class, 'spec'])->name('swagger.spec');
