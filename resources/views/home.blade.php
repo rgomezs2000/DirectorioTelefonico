c6e4af9e75245ba649411b826efa3e94f1258cb3
Roger Alejandro Gomez Sanabria <rogergomezs2003@gmail.com>
2026-03-24 17:56:37 -0400

    Make home image centered and fully responsive without cropping

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
     <section class="p-4 md:p-6 lg:p-8">
-        <div class="mx-auto h-[360px] w-full max-w-5xl overflow-hidden rounded-md shadow-sm md:h-[560px] lg:h-[760px]">
+        <div class="mx-auto flex w-full max-w-5xl items-center justify-center rounded-md">
             <img src="{{ asset('img/directorio.png') }}"
                  alt="Logo"
-                 class="w-full h-full object-cover">
+                 class="block h-auto w-full max-h-[78vh] object-contain object-center">
         </div>
     </section>
 @endsection
