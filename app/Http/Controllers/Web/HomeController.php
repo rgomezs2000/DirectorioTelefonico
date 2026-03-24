c1de50363b8dfc3e151542a963c5c46a18607476
Roger Alejandro Gomez Sanabria <rogergomezs2003@gmail.com>
2026-03-24 03:22:53 -0400

    Inicializa pool de sesiones en HomeController

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -15,6 +15,15 @@ class HomeController extends Controller
             return redirect()->route('login');
         }
 
+        if (! $request->session()->has('session_pool')) {
+            $request->session()->put('session_pool', [
+                'initialized_at' => now()->toIso8601String(),
+                'current_session_id' => $request->session()->getId(),
+                'login_events' => [],
+                'transaction_events' => [],
+            ]);
+        }
+
         return view('home');
     }
 }
